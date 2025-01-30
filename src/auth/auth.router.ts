4736dcd89725a63f115bfa5c2d18c9dae889c63a
itolkachev <vantrey@yandex.ru>
2025-01-30 15:47:13 +0300

    refactoring

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,17 +1,17 @@
-import { Response, Router } from "express";
-import { RequestWithBody, RequestWithUserId } from "../common/types/requests";
-import { LoginInputDto } from "./types/login.input.dto";
-import { authService } from "./auth.service";
-import { routersPaths } from "../common/path/paths";
-import { passwordValidation } from "../users/middlewares/password.validation";
-import { inputValidation } from "../common/validation/input.validation";
-import { loginOrEmailValidation } from "../users/middlewares/login.or.emaol.validation";
-import { accessTokenGuard } from "./guards/access.token.guard";
-import { usersQwRepository } from "../users/user.query.repository";
-import { IdType } from "../common/types/id";
-import { ResultStatus } from "../common/result/resultCode";
-import { resultCodeToHttpException } from "../common/result/resultCodeToHttpException";
-import { HttpStatuses } from "../common/types/httpStatuses";
+import { Response, Router } from 'express';
+import { RequestWithBody, RequestWithUserId } from '../common/types/requests';
+import { accessTokenGuard } from './guards/access.token.guard';
+import { IdType } from '../common/types/id';
+import { ResultStatus } from '../common/result/resultCode';
+import { resultCodeToHttpException } from '../common/result/resultCodeToHttpException';
+import { LoginInputDto } from './types/login.input.dto';
+import { authService } from './auth.service';
+import { routersPaths } from '../common/path/paths';
+import { passwordValidation } from '../users/api/middlewares/password.validation';
+import { inputValidation } from '../common/validation/input.validation';
+import { loginOrEmailValidation } from '../users/api/middlewares/login.or.emaol.validation';
+import { HttpStatuses } from '../common/types/httpStatuses';
+import { usersQwRepository } from '../users/infrastructure/user.query.repository';
 
 export const authRouter = Router();
 
@@ -26,14 +26,10 @@ authRouter.post(
     const result = await authService.loginUser(loginOrEmail, password);
 
     if (result.status !== ResultStatus.Success) {
-      return res
-        .status(resultCodeToHttpException(result.status))
-        .send(result.extensions);
+      return res.status(resultCodeToHttpException(result.status)).send(result.extensions);
     }
 
-    return res
-      .status(HttpStatuses.Success)
-      .send({ accessToken: result.data!.accessToken });
+    return res.status(HttpStatuses.Success).send({ accessToken: result.data!.accessToken });
   },
 );
 
