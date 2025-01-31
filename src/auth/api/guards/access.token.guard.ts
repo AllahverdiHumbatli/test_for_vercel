f903d11d85ec1654d6067864e77a9232f6d00b1e
itolkachev <vantrey@yandex.ru>
2025-01-31 14:56:41 +0300

    refactoring guard

--- src/auth/api/guards/access.token.guard.ts
+++ src/auth/api/guards/access.token.guard.ts
@@ -1,28 +1,24 @@
-import {NextFunction, Request, Response} from "express";
-import {IdType} from "../../common/types/id";
-import {jwtService} from "../../common/adapters/jwt.service";
-import {usersRepository} from "../../users/user.repository";
+import { NextFunction, Request, Response } from 'express';
+import { jwtService } from '../../adapters/jwt.service';
+import { IdType } from '../../../common/types/id';
 
-export const accessTokenGuard = async (req: Request,
-                                       res: Response,
-                                       next: NextFunction) => {
-    if (!req.headers.authorization) return res.sendStatus(401);
+export const accessTokenGuard = async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.headers.authorization) return res.sendStatus(401);
 
-    const [authType, token] = req.headers.authorization.split(" ")[1];
+  const [authType, token] = req.headers.authorization.split(' ')[1];
 
-    if (authType !== 'Bearer') return res.sendStatus(401);
+  if (authType !== 'Bearer') return res.sendStatus(401);
 
-    const payload = await jwtService.verifyToken(token);
-    if (payload) {
-        const {userId} = payload;
+  const payload = await jwtService.verifyToken(token);
+  if (payload) {
+    const { userId } = payload;
 
-        const user = await usersRepository.doesExistById(userId);
+    req.user = { id: userId } as IdType;
+    next();
 
-        if (!user) return res.sendStatus(401);
+    return;
+  }
+  res.sendStatus(401);
 
-        req.user = {id: userId} as IdType;
-        return next();
-    }
-    return res.sendStatus(401);
-
-}
+  return;
+};
